753e64fe67501f2487ba8f5ed3c5337764ec49cb
Your Name <kdima031006@gmail.com>
2024-03-08 15:14:34 +0600

    btn two

--- src/apps/Layout/advantages/Advantages.js
+++ src/apps/Layout/advantages/Advantages.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 const Advantages = () => {
@@ -10,17 +9,4 @@ const Advantages = () => {
     );
 };
 
-=======
-import React from 'react';
-
-const Advantages = () => {
-    return (
-        <div>
-            <h1>Преимущества</h1>
-            <button>create</button>
-        </div>
-    );
-};
-
->>>>>>> 0a020d73b2c3a777846748724be4355d9cb2b97c
 export default Advantages;
