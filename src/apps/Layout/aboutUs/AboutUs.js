cc14703106aa83d1066a84328a18bb7530c85f52
Chyngyz <142288166+AlmazovChyngyz@users.noreply.github.com>
2024-03-09 17:37:51 +0600

    Fixed bags

--- src/apps/Layout/aboutUs/AboutUs.js
+++ src/apps/Layout/aboutUs/AboutUs.js
@@ -1,10 +1,14 @@
 import React from 'react';
 // import 'react-redux'
 // import MainAboutUp from '../mainAbout/MainAboutUp';
-import HistoryAdd from '../historyAdd/HistoryAdd'
-import OurValues from "../ourValues/OurValues";
-import WorkBlock from "../work/WorkBlock";
-import Qualities from "../Qualities/Qualities";
+// import HistoryAdd from '../historyAdd/HistoryAdd'
+// import OurValues from "../ourValues/OurValues";
+// import WorkBlock from "../work/WorkBlock";
+// import Qualities from "../Qualities/Qualities";
+import Qualities from "./blocks/Qualities/Qualities";
+import WorkBlock from "./blocks/work/WorkBlock";
+import OurValues from "./blocks/ourValues/OurValues";
+import HistoryAdd from "./blocks/historyAdd/HistoryAdd";
 // import OurTeam from "./blocks/ourTeam/OurTeam";
 
 
